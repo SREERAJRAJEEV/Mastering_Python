fa875ef86c1d6951df5fe5b94296130112944216
SREERAJRAJEEV <sreerajrajeev28@gmail.com>
2024-07-11 08:02:41 +0530

    Create and add code in  words.py

---
+++ Hangman Game/words.py
@@ -0,0 +1,53 @@
+"""
+words.py
+
+This module contains a list of words used in the Hangman game. Each word in the list is a possible word that the player has to guess. The words are chosen to provide a variety of lengths and difficulties, ensuring an engaging and challenging game experience.
+
+Modules:
+    None
+
+Attributes:
+    wordlist (list): A list of strings, each representing a word used in the Hangman game.
+
+Usage:
+    This module is imported by `main.py` to provide the pool of words from which a random word is selected for the game.
+
+Example:
+    Importing the wordlist in another script:
+        from words import wordlist
+
+Note:
+    You can modify the `wordlist` to include additional words or remove words as desired.
+"""
+wordlist = [
+    "Adventure",
+    "Bicycle",
+    "Chocolate",
+    "Dolphin",
+    "Elephant",
+    "Friendship",
+    "Galaxy",
+    "Hamburger",
+    "Imagination",
+    "Jellyfish",
+    "Kangaroo",
+    "Lemonade",
+    "Mountain",
+    "Notebook",
+    "Octopus",
+    "Pineapple",
+    "Quicksilver",
+    "Rainbow",
+    "Sunflower",
+    "Treasure",
+    "Universe",
+    "Volcano",
+    "Waterfall",
+    "Xylophone",
+    "Yacht",
+    "Zephyr",
+    "Butterfly",
+    "Campfire",
+    "Dragonfly",
+    "Eclipse"
+]
